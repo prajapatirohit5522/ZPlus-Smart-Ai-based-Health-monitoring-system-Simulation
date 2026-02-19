698aaf557971be2226a7ec2bc3c0552ccebfaa52
Roshan Kumar <prajapatirohit5522@gmail.com>
2026-02-19 22:29:34 +0530

    Initial project setup with core and configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zsimulation.app',
+  appName: 'zsimulation',
+  webDir: 'dist'
+};
+
+export default config;
